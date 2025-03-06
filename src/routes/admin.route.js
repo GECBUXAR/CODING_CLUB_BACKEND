d827f6c585ec1f26045e98f21ac1ff0faac5bf13
Azlan <azlanehtesham02@gmail.com>
2025-03-06 22:00:16 +0530

    fix

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { createAdmin, loginAdmin } from "../controller/admin.controller.js";
+import {
+  createAdmin,
+  loginAdmin,
+  logOutAdmin,
+} from "../controller/admin.controller.js";
 const router = express.Router();
 
 router.post("/signup", createAdmin);
 router.post("/login", loginAdmin);
-router.post("/logout", loginAdmin);
+router.post("/logout", logOutAdmin);
 
 export default router;
