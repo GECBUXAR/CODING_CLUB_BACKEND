2a04697c8c12eff8124a1ce8bd8c925cf351fa11
Keshav raj <itsrajkeshav@gmail.com>
2025-03-08 22:26:49 +0530

    update testimonial

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
-import { ApiError } from "../utils/ApiError.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
-import { Users } from "../models/users.model.js";
+import  ApiError  from "../utils/ApiError.js";
+import  asyncHandler  from "../utils/asyncHandler.js";
+import  Users  from "../model/user.model.js";
 import jwt from "jsonwebtoken";
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
